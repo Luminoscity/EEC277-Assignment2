c8b2fa09b790803fe98e5c7b0369335a2e040409
Tim Ambrose <Tim Ambrose>
2017-02-05 20:45:14 -0800

    Removed unneeded function declaration.

--- TestFloatingPointPrecision.cpp
+++ TestFloatingPointPrecision.cpp
@@ -43,7 +43,6 @@ static void error_callback(int error, const char* description);
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void printInfo(GLFWwindow * window);
 void runTest(int test, GLFWwindow *window, vector<GLuint> *shaders, vector<string> *shaderNames);
-void runLargeIntegerRepresentationTest(GLuint doubleProgram, GLuint floatProgram);
 #pragma endregion
 
 #pragma region Main
